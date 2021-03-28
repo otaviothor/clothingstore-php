007c8415004c2a593faf02ef0e3d5a68f496764d
thor <otaviosilva2632@gmail.com>
2021-03-27 23:38:37 -0300

    feat: refactoring js to () => {}

--- views/assets/js/login.js
+++ views/assets/js/login.js
@@ -1,37 +1,39 @@
-let step = 1;
+$(() => {
+  let step = 1;
 
-$(".next-step").on("click", (e) => {
-  e.preventDefault();
-  switch (step) {
-    case 1:
-      $(".step-1").removeClass("d-block").addClass("d-none");
-      $(".step-2").removeClass("d-none").addClass("d-block");
-      break;
-    case 2:
-      $(".step-2").removeClass("d-block").addClass("d-none");
-      $(".step-3").removeClass("d-none").addClass("d-block");
-      break;
-  }
-  step++;
-});
+  $(".next-step").on("click", (e) => {
+    e.preventDefault();
+    switch (step) {
+      case 1:
+        $(".step-1").removeClass("d-block").addClass("d-none");
+        $(".step-2").removeClass("d-none").addClass("d-block");
+        break;
+      case 2:
+        $(".step-2").removeClass("d-block").addClass("d-none");
+        $(".step-3").removeClass("d-none").addClass("d-block");
+        break;
+    }
+    step++;
+  });
 
-$(".prev-step").on("click", (e) => {
-  e.preventDefault();
-  switch (step) {
-    case 2:
-      $(".step-1").removeClass("d-none").addClass("d-block");
-      $(".step-2").removeClass("d-block").addClass("d-none");
-      break;
-    case 3:
-      $(".step-2").removeClass("d-none").addClass("d-block");
-      $(".step-3").removeClass("d-block").addClass("d-none");
-      break;
-  }
-  step--;
-});
+  $(".prev-step").on("click", (e) => {
+    e.preventDefault();
+    switch (step) {
+      case 2:
+        $(".step-1").removeClass("d-none").addClass("d-block");
+        $(".step-2").removeClass("d-block").addClass("d-none");
+        break;
+      case 3:
+        $(".step-2").removeClass("d-none").addClass("d-block");
+        $(".step-3").removeClass("d-block").addClass("d-none");
+        break;
+    }
+    step--;
+  });
 
-$(".btn-recover-password").on("click", (e) => {
-  e.preventDefault();
-  $(".form-login").toggleClass("d-none");
-  $(".form-recover-password").toggleClass("d-none");
-});
+  $(".btn-recover-password").on("click", (e) => {
+    e.preventDefault();
+    $(".form-login").toggleClass("d-none");
+    $(".form-recover-password").toggleClass("d-none");
+  });
+})
--- views/assets/js/scripts.js
+++ views/assets/js/scripts.js
@@ -1,24 +1,25 @@
-$(".btn-show-password").on("click", () => {
-  $(".btn-show-password-icon").toggleClass("fa-eye fa-eye-slash");
-  if ($("#password").attr("type") == "password") {
-    $("#password").attr("type", "text");
-  } else {
-    $("#password").attr("type", "password");
-  }
-});
-
-$(".upload-image").on("change", (e) => {
-  const files = e.target.files;
-  if (files && files[0]) {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      $(".image-preview").attr("src", reader.result);
-    };
-    reader.readAsDataURL(files[0]);
-  }
-});
-
 $(() => {
-  $('[data-toggle="tooltip"]').tooltip();
-});
+  $(".btn-show-password").on("click", () => {
+    $(".btn-show-password-icon").toggleClass("fa-eye fa-eye-slash");
+    if ($("#password").attr("type") == "password") {
+      $("#password").attr("type", "text");
+    } else {
+      $("#password").attr("type", "password");
+    }
+  });
 
+  $(".upload-image").on("change", (e) => {
+    const files = e.target.files;
+    if (files && files[0]) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        $(".image-preview").attr("src", reader.result);
+      };
+      reader.readAsDataURL(files[0]);
+    }
+  });
+
+  $(() => {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+});
