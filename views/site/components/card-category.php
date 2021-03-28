3f3b1e65af258a7e4759a5596e223f2e28cb23e1
thor <otaviosilva2632@gmail.com>
2021-03-27 23:23:32 -0300

    feat: listing registered categories

--- views/site/components/card-category.php
+++ views/site/components/card-category.php
@@ -1,10 +1,10 @@
 <div class="col-sm-12 col-md-4 col-lg-3 my-3">
   <div class="card">
-    <div class="card-body">
-      <h3 class="card-title"><?= $category["category"] ?></h3>
+    <div class="card-body d-flex justify-content-between">
+      <h4 class="card-title"><?= $category["category"] ?></h4>
       <div>
-        <a href="<?= $router->route("category.show", ["id" => $category["id"]]) ?>"><i class="far fa-trash-alt"></i></a>
-        <a href="<?= $router->route("category.show", ["id" => $category["id"]]) ?>"><i class="far fa-edit"></i></i></a>
+        <a href="<?= $router->route("category.show", ["id" => $category["id"]]) ?>" class="text-dark mr-1"><i class="far fa-trash-alt"></i></a>
+        <a href="<?= $router->route("category.show", ["id" => $category["id"]]) ?>" class="text-dark"><i class="far fa-edit"></i></i></a>
       </div>
     </div>
   </div>
