e663625f7ad1f064ab3fadba6d010b92ddd927eb
thor <otaviosilva2632@gmail.com>
2021-03-27 23:28:09 -0300

    feat: add dflex on category card icons

--- views/site/components/card-category.php
+++ views/site/components/card-category.php
@@ -2,7 +2,7 @@
   <div class="card shadow">
     <div class="card-body d-flex justify-content-between">
       <h4 class="card-title"><?= $category["category"] ?></h4>
-      <div>
+      <div class="d-flex">
         <a href="<?= $router->route("category.show", ["id" => $category["id"]]) ?>" class="text-dark mr-1"><i class="far fa-trash-alt"></i></a>
         <a href="<?= $router->route("category.show", ["id" => $category["id"]]) ?>" class="text-dark"><i class="far fa-edit"></i></i></a>
       </div>
